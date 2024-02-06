ff44668513562fc954ff8df67a5194eb238d7da5
noname1818271718172672 <127699732+ixzzxitado@users.noreply.github.com>
2024-02-06 10:45:01 -0300

    Add files via upload

---
+++ own.bash
@@ -0,0 +1,10 @@
+pkg install ssh
+pkg install expect
+sshd
+read -p "Type the HOSTED IP: " ip
+read -p "Type the HOSTED USER: " user
+echo -e "ssh $user@$ip -p 8022" >> sshopen.sh
+chmod +x *
+echo "#!usr/bin/expect" >> ilovejesus.exp
+echo -e "spawn ./sshopen.sh\n \n send -- "4231\r" \n \n expect "%" \n"  >> ilovejesus.exp
+expect ilovejesus.exp
